0670ff88e82847dde4d5e4a38434f70455ed8e2d
isagusmao97 <isagusmao93@gmail.com>
2025-10-06 21:48:11 -0300

    atualização de cores

--- lib/App_colors.dart
+++ lib/App_colors.dart
@@ -3,13 +3,14 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF414C9E);    // Azul principal
-  static const Color secondary = Color(0xFFA974A0);  // Roxo secundário
-  static const Color accent = Color(0xFFCF7486);     // Rosa destaque
+  static const Color primary = Color(0xFF26C6DA);    // Azul
+  static const Color secondary = Color(0xFF5E35B1);  // Roxo 
+  static const Color accent = Color(0xFFEC407A);     // Rosa 
   static const Color background = Color(0xFFEDEBDD); // Creme claro de fundo
   static const Color textPrimary = Color(0xFF0E0204); // Preto para textos
   static const Color textSecondary = Color(0xFF404040); // Cinza para textos secundários
   static const Color textLight = Color(0xFFEDEBDD); // Creme para textos claros
-  static const Color avaliacao = Color(0xFFFFBE55); // Amarelo para avaliação
-  static const Color cinza2 = Color(0xFFBFBFBF); // Cinza claro 
+  static const Color teal = Color(0xFF26A69A); // Teal
+  static const Color green = Color(0xFF66BB6A); // Verde
+  static const Color cinza2 = Color(0xFF90A4AE); // Cinza claro 
 }
